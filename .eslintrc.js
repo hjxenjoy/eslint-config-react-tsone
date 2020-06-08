913f71035f9258f9dcc852ad439be224ff6fd3b4
Jinxin Hu <hjxenjoy@gmail.com>
2020-06-08 16:58:47 +0800

    improvement: update rc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,30 @@
 module.exports = {
-  "extends": [
-    "react-app",
-    "airbnb",
-    "prettier",
-    "prettier/react",
-  ],
-  "plugins": [
-    "prettier",
-  ],
-  "rules": {
+  extends: ["react-app", "airbnb", "prettier", "prettier/react"],
+  plugins: ["prettier"],
+  rules: {
     "prettier/prettier": "error",
-    "react/jsx-filename-extension": ["warn", {
-      "extensions": [".ts", ".tsx"]
-    }],
-    "react/jsx-props-no-spreading": 0,
-    "react/prop-types": 0,
-    "react/button-has-type": 0
+    "react/jsx-filename-extension": [
+      "warn",
+      {
+        extensions: [".ts", ".tsx"],
+      },
+    ],
+    "react/jsx-props-no-spreading": "off",
+    "react/prop-types": "off",
+    "react/button-has-type": "off",
   },
-  "settings": {
+  settings: {
     "import/resolver": {
-      "node": {
-        "extensions": [".js", ".jsx", ".ts", ".tsx"]
-      }
-    }
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
   },
-  "globals": {
-    "window": true,
-    "document": true,
-    "FormData": true,
-    "FileReader": true,
-    "Blob": true,
+  globals: {
+    window: true,
+    document: true,
+    FormData: true,
+    FileReader: true,
+    Blob: true,
   },
-}
+};
